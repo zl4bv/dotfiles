1887d05954583fc900d911d95fde36f1d3b5c50e
Ben Vidulich <ben@vidulich.nz>
2019-04-29 23:31:34 +1000

    Fix permissions

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ if [ "$(uname -s)" != "Darwin" ]; then
 fi
 
 # add aliases for dotfiles
-for file in $(shell find "${CURDIR}" -name ".*" -not -name ".gitignore" -not -name ".travis.yml" -not -name ".git" -not -name ".*.swp" -not -name ".gnupg"); do
+for file in $(find "${CURDIR}" -name ".*" -not -name ".gitignore" -not -name ".travis.yml" -not -name ".git" -not -name ".*.swp" -not -name ".gnupg"); do
   f=$(basename "${file}")
   ln -sfn "${file}" "${HOME}/${f}"
 done
