517186dcebe4f62e3c0108ac214e173ee9ecfc2a
Ben Vidulich <ben@vidulich.nz>
2021-03-09 11:38:13 +1100

    hardware model detection for mac-based platforms

--- detect-hw.sh
+++ detect-hw.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-if [ -f /sys/devices/virtual/dmi/id/product_name ]; then
-  HW_MODEL=$(< /sys/devices/virtual/dmi/id/product_name)
-fi
+DOTFILESDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# shellcheck disable=SC1090
+source "${DOTFILESDIR}/detect-os.sh"
+
+case "${OS_PLATFORM}" in
+  linux)
+    if [ -f /sys/devices/virtual/dmi/id/product_name ]; then
+      HW_MODEL=$(< /sys/devices/virtual/dmi/id/product_name)
+    fi
+    ;;
+
+  mac)
+    HW_MODEL=$(sysctl -n hw.model)
+    ;;
+esac
 export HW_MODEL
