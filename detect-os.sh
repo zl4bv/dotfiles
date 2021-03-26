090ebfce10062c33757e8173e45aeb9d6977f3c2
Ben Vidulich <ben@vidulich.nz>
2021-03-26 15:15:31 +1100

    Improve macOS detection

--- detect-os.sh
+++ detect-os.sh
@@ -6,6 +6,8 @@ case "${kernel_name}" in
     OS_PLATFORM=linux
 
     if [ -f /etc/os-release ]; then
+      OS_NAME=$(bash -c 'source /etc/os-release; echo ${NAME}')
+      OS_VERSION=$(bash -c 'source /etc/os-release; echo ${VERSION}')
       OS_ID=$(bash -c 'source /etc/os-release; echo ${ID}')
       OS_ID_LIKE=$(bash -c 'source /etc/os-release; echo ${ID_LIKE}')
     fi
@@ -13,6 +15,8 @@ case "${kernel_name}" in
 
   Darwin*)
     OS_PLATFORM=mac
+    OS_NAME=$(sw_vers -productName)
+    OS_VERSION=$(sw_vers -productVersion)
     ;;
 
   *)
@@ -20,5 +24,8 @@ case "${kernel_name}" in
     ;;
 esac
 export OS_PLATFORM
+export OS_NAME
+export OS_VERSION
 export OS_ID
 export OS_ID_LIKE
+
