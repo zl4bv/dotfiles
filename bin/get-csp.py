40bcfee463bf29e5fa5b0a6b5c33d1cc2aa89083
Ben Vidulich <ben@vidulich.nz>
2021-04-23 17:02:25 +1000

    fix flake8 offenses

--- bin/get-csp.py
+++ bin/get-csp.py
@@ -6,40 +6,43 @@ import urllib.request
 CSP_HEADER = 'Content-Security-Policy'
 CSP_REPORT_ONLY_HEADER = 'Content-Security-Policy-Report-Only'
 
+
 def main(argv):
-  url = argv[0]
+    url = argv[0]
 
-  if not (url.startswith('http://') or url.startswith('https://')):
-    url = 'https://' + url
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = 'https://' + url
 
-  with urllib.request.urlopen(url) as response:
-    if response.headers[CSP_HEADER]:
-      csp = response.headers[CSP_HEADER]
-      disposition = 'enforce'
-    elif response.headers[CSP_REPORT_ONLY_HEADER]:
-      csp = response.headers[CSP_REPORT_ONLY_HEADER]
-      disposition = 'report-only'
-    else:
-      print('error: CSP header not found in response')
-      return
+    with urllib.request.urlopen(url) as response:
+        if response.headers[CSP_HEADER]:
+            csp = response.headers[CSP_HEADER]
+            disposition = 'enforce'
+        elif response.headers[CSP_REPORT_ONLY_HEADER]:
+            csp = response.headers[CSP_REPORT_ONLY_HEADER]
+            disposition = 'report-only'
+        else:
+            print('error: CSP header not found in response')
+            return
 
-    directives = {}
-    for policy_directive in csp.split(';'):
-      parts = policy_directive.strip().split(' ')
-      directives[parts[0]] = parts[1:]
+        directives = {}
+        for policy_directive in csp.split(';'):
+            parts = policy_directive.strip().split(' ')
+            directives[parts[0]] = parts[1:]
 
-    print('disposition: %s' % disposition)
-    for directive, values in directives.items():
-      print(directive)
-      for value in sorted(values):
-        print("\t%s" % value)
+        print('disposition: %s' % disposition)
+        for directive, values in directives.items():
+            print(directive)
+            for value in sorted(values):
+                print("\t%s" % value)
+
 
 def usage(argv):
-  print('usage: %s url')
+    print('usage: %s url')
+
 
 if __name__ == '__main__':
-  if len(sys.argv) != 2:
-    usage(sys.argv)
-    exit(1)
+    if len(sys.argv) != 2:
+        usage(sys.argv)
+        exit(1)
 
-  main(sys.argv[1:])
+    main(sys.argv[1:])
