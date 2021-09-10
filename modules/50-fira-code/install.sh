7034812c18ae1976f215dbed982c82d21f4f00a1
Ben Vidulich <ben@vidulich.nz>
2021-09-10 13:57:45 +1000

    install firacode font on debian-based systems

--- modules/50-fira-code/install.sh
+++ modules/50-fira-code/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 case "${PKG_MGR}" in
+  apt-get)
+    sudo apt-get install --yes fonts-firacode
+    ;;
   homebrew)
     brew install font-fira-code
     ;;
   pacman)
     pacman -S ttf-fira-code
+    ;;
+  *)
+    echo "Warning! Not installing FiraCode font: unknown package manager"
     ;;
 esac
