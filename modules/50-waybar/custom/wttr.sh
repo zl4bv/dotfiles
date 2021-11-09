49bfabfca51c244e2e6759abb209fedb9f403be1
Ben Vidulich <ben@vidulich.nz>
2021-11-09 22:39:23 +1100

    show when last updated

--- modules/50-waybar/custom/wttr.sh
+++ modules/50-waybar/custom/wttr.sh
@@ -2,5 +2,7 @@
 
 text=$(curl -s 'https://wttr.in/?format=1' | tr -d '+')
 tooltip=$(curl -s 'https://wttr.in/?format=4')
+lastupdated=$(date +%R)
 
-echo "{\"text\":\"${text}\",\"tooltip\":\"${tooltip//$'\n'/\\n}\"}"
+# shellcheck disable=SC2028
+echo "{\"text\":\"${text}\",\"tooltip\":\"${tooltip//$'\n'/\\n}\\nLast updated: ${lastupdated}\"}"
