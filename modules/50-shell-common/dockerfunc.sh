d3e602d1575de75825a0af3739cac01a8926f8f7
Ben Vidulich <ben.vidulich@medipass.io>
2022-10-07 09:51:19 +1100

    remove unused functions

--- modules/50-shell-common/dockerfunc.sh
+++ modules/50-shell-common/dockerfunc.sh
@@ -1,20 +1,2 @@
 #!/bin/sh
 
-#shellcheck(){
-#    docker run -it --rm \
-#	-v "$(pwd):/usr/src:ro" \
-#	-w /usr/src \
-#	r.j3ss.co/shellcheck shellcheck "$@"
-#}
-
-#terraform(){
-#    docker run -it --rm \
-#        -v "$(pwd):/tf" \
-#        -w /tf \
-#        -e AWS_SESSION_TOKEN \
-#        -e AWS_SECRET_ACCESS_KEY \
-#        -e AWS_ACCESS_KEY_ID \
-#        -e CLOUDFLARE_API_TOKEN \
-#        -e TF_LOG \
-#        r.j3ss.co/terraform "$@"
-#}
