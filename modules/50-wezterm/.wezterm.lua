901bb1e4192d2b6958dae77ad4afa50913025d01
Ben Vidulich <ben.vidulich@medipass.io>
2023-06-27 09:57:28 +1000

    [wezterm] tweak appearance

--- modules/50-wezterm/.wezterm.lua
+++ modules/50-wezterm/.wezterm.lua
@@ -3,23 +3,13 @@ local config = {}
 
 config.color_scheme = 'Night Owl (Gogh)'
 config.font = wezterm.font 'Fira Code'
+config.window_padding = {
+  left = '2cell',
+  right = '2cell',
+  top = '1cell',
+  bottom = '1cell',
+}
 
-function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Night Owl (Gogh)"
-  else
-    return "Night Owlish Light"
-  end
-end
-
-wezterm.on("window-config-reloaded", function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-  local appearance = window:get_appearance()
-  local scheme = scheme_for_appearance(appearance)
-  if overrides.color_scheme ~= scheme then
-    overrides.color_scheme = scheme
-    window:set_config_overrides(overrides)
-  end
-end)
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
