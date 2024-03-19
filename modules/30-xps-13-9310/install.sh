a0339bd099cf96a0c5ee9b11c9dceb5c2f7e4bd9
Ben Vidulich <ben@vidulich.nz>
2024-03-19 20:51:55 +1100

    [xps-13-9310] add keyboard brightness service

--- modules/30-xps-13-9310/install.sh
+++ modules/30-xps-13-9310/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CURDIR="${HOME}/.dotfiles/modules/30-xps-13-9310"
+
 if [ "${HW_MODEL}" != "XPS 13 9310" ]; then
   exit 0
 fi
@@ -9,3 +11,23 @@ case "${PKG_MGR}" in
     sudo pacman -R xf86-video-intel
     ;;
 esac
+
+RELOAD_REQUIRED=false
+if [ -f "/etc/systemd/system/keyboard-brightness.service" ]; then
+  old_hash=$(shasum -a 256 "/etc/systemd/system/keyboard-brightness.service" | awk '{print $1}')
+  new_hash=$(shasum -a 256 "${CURDIR}/keyboard-brightness.service" | awk '{print $1}')
+
+  if [ "${old_hash}" != "${new_hash}" ]; then
+    sudo cp "${CURDIR}/keyboard-brightness.service" "/etc/systemd/system/keyboard-brightness.service"
+    RELOAD_REQUIRED=true
+  fi
+else
+  sudo cp "${CURDIR}/keyboard-brightness.service" "/etc/systemd/system/keyboard-brightness.service"
+  RELOAD_REQUIRED=true
+fi
+
+if [ "${RELOAD_REQUIRED}" = "true" ]; then
+  sudo systemctl daemon-reload
+  sudo systemctl enable keyboard-brightness.service
+  sudo systemctl restart keyboard-brightness.service
+fi
