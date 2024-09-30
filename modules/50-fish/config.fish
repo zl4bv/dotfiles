47b89741206595481235c1faa3e68a6333cccd63
Ben Vidulich <ben.vidulich@medipass.io>
2024-09-30 11:37:15 +1000

    bring !! and !$ to fish

--- modules/50-fish/config.fish
+++ modules/50-fish/config.fish
@@ -1,5 +1,30 @@
 #!/usr/bin/env fish
 
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+# Bash-friendly alternative to Alt + Up
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
